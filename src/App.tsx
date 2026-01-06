8a78393ec8361f85df1630df16504dd66077f197
Atasay Mansurgüler <atasaymguler@gmail.com>
2026-01-06 13:13:00 +0300

    Theme eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useSelector } from "react-redux"
 import Asidebar from "./components/Asidebar"
 import Navbar from "./components/Navbar"
+import type { RootState } from "./store/store"
+import { useEffect } from "react"
 
 function App() {
 
+  const darkTheme = useSelector((state:RootState)=> state.app.darkTheme)
+  useEffect(()=> {
+    document.documentElement.classList.toggle("dark",darkTheme)
+  },[darkTheme])
   return (
-    <div className="flex">
+    <div className="flex ">
     <Asidebar />
      <Navbar />
     </div>
--- src/store/slice/appSlice.ts
+++ src/store/slice/appSlice.ts
@@ -1,21 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export interface AppState {
-
+  darkTheme: boolean;
 }
 
 const initialState: AppState = {
-
-}
+  darkTheme: false,
+};
 
 export const appSlice = createSlice({
-  name: 'app',
+  name: "app",
   initialState,
   reducers: {
-    
+    openDarkTheme: (state: AppState) => {
+      state.darkTheme = true;
+    },
+    closeDarkTheme: (state: AppState) => {
+      state.darkTheme = false;
+    },
   },
-})
+});
 
-export const {  } = appSlice.actions
+export const { openDarkTheme, closeDarkTheme } = appSlice.actions;
 
-export default appSlice.reducer
+export default appSlice.reducer;
